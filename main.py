2cbee100b9d60bff38fd9ab9d1636517b3921cf7
Barcalegion <excarevo@gmail.com>
2022-04-21 16:48:48 -0700

    deal cards function

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import queue as q
 
 r.seed()
 
-def shuffleDeck(deck):
-    count = 0
+NUM_PLAYERS = 2
+
+PLAYERPOCKETS = {}
+
+COMMUNITYCARDS = []
+
+COMBINEDCARDS = {}
+
+WINNINGPLAYERS = []
+
+def shuffleDeck(deck):  
     
     cards = 51
 
@@ -15,9 +24,26 @@ def shuffleDeck(deck):
         shuffledDeck.put(deck[randomCard])
         cards = cards - 1
         del deck[randomCard]
-        
+
     return shuffledDeck
 
+def deal_cards(deck):    
+    for i in range(NUM_PLAYERS):
+        PLAYERPOCKETS.update({i : [deck.get(),deck.get()]})
+
+    for i in range(5):
+        COMMUNITYCARDS.append(deck.get())
+
+def combine_cards(playerCards, communityCards):
+    for i in range(NUM_PLAYERS):
+        for y in range(7):
+            if y < 2:
+                COMBINEDCARDS.update({i: [playerCards.pop(i)]})
+            else:
+                COMBINEDCARDS.update({i: [communityCards.pop(i - 2)]})
+        
+        
+            
 suits = ["clubs","hearts","Diamonds","Spades"]
 
 values = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"]
@@ -32,5 +58,12 @@ for i in range(len(values)):
 
 readyDeck = shuffleDeck(Deck)
 
-while not readyDeck.empty():
-    print(readyDeck.get())
+deal_cards(readyDeck)
+
+combine_cards(PLAYERPOCKETS, COMMUNITYCARDS)
+
+print("Player pocket cards: ",PLAYERPOCKETS)
+
+print("Community cards: ", COMMUNITYCARDS)
+
+print("Combined cards: ", COMBINEDCARDS)
